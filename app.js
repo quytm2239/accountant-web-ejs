41b11483ac50ad11f52d970cc76a1446916b8b8e
quytm2239 <quytm2239@gmail.com>
2018-06-17 01:25:14 +0700

    Update init

--- app.js
+++ app.js
@@ -60,12 +60,18 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  // res.locals.message = err.message;
+  // res.locals.error = req.app.get('env') === 'development' ? err : {};
+  //
+  // // render the error page
+  // res.status(err.status || 500);
+  // res.render('error');
+  if (err.status == 404) {
+    res.redirect('/404.html')
+  }
+  if (err.status == 500) {
+    res.redirect('/500.html')
+  }
 });
 
 module.exports = app;
