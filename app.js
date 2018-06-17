0f5a9902d724ae6e4cb4829826e778f00cd5e83d
quytm2239 <quytm2239@gmail.com>
2018-06-17 23:16:23 +0700

    Update 404 and 500 page

--- app.js
+++ app.js
@@ -19,24 +19,25 @@ var redisStore = require('connect-redis')(session);
 var redisClient = redis.createClient();
 
 app.use(session({
-    secret: 'node-mvc-demo',
-    // cookie: { maxAge: 3000 },
-    // create new redis store.
-    store: new redisStore({
-        host: 'localhost',
-        port: 6379,
-        client: redisClient,
-        ttl :  180}),
-    resave: false,
-    saveUninitialized: false,
-    rolling: true
+	secret: 'node-mvc-demo',
+	// cookie: { maxAge: 3000 },
+	// create new redis store.
+	store: new redisStore({
+		host: 'localhost',
+		port: 6379,
+		client: redisClient,
+		ttl: 180
+	}),
+	resave: false,
+	saveUninitialized: false,
+	rolling: true
 }));
 
 // get our predefined file
 var show_clientip = require('./middleware/show_clientip')
 var check_session = require('./middleware/check_session')
 app.use(show_clientip)
-app.set('check_session',check_session)
+app.set('check_session', check_session)
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -45,7 +46,9 @@ app.use(ejsLayouts);
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+	extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -54,24 +57,28 @@ app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+	next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  // res.locals.message = err.message;
-  // res.locals.error = req.app.get('env') === 'development' ? err : {};
-  //
-  // // render the error page
-  // res.status(err.status || 500);
-  // res.render('error');
-  if (err.status == 404) {
-    res.redirect('/404.html')
-  }
-  if (err.status == 500) {
-    res.redirect('/500.html')
-  }
+	// set locals, only providing error in development
+	// res.locals.message = err.message;
+	// res.locals.error = req.app.get('env') === 'development' ? err : {};
+	//
+	// // render the error page
+	// res.status(err.status || 500);
+	// res.render('error');
+	if (err.status == 404) {
+		res.render('err-page/404', {
+			layout: 'plain-layout'
+		});
+	}
+	if (err.status == 500) {
+		res.render('err-page/500', {
+			layout: 'plain-layout'
+		});
+	}
 });
 
 module.exports = app;
