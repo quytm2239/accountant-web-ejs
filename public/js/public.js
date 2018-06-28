cf9a3ae462e2264a9086530d3c9edb84af4381bb
quytm2239 <quytm2239@gmail.com>
2018-06-28 16:20:58 +0700

    Update logic

--- public/js/public.js
+++ public/js/public.js
@@ -112,4 +112,23 @@ $(function() {
             }
         });
     });
+
+    function centerModal() {
+        // $(this).prop('src', );
+        $(this).css('display', 'block');
+        var $dialog = $(this).find(".modal-dialog");
+        var offset = ($(window).height() - $dialog.height()) / 2;
+        // Center modal vertically in window
+        $dialog.css("margin-top", offset);
+    }
+
+    $('.modal').on('show.bs.modal', centerModal);
+    $(window).on("resize", function () {
+        $('.modal:visible').each(centerModal);
+    });
+
+    // $('img').on('click',function() {
+    //     $('#myModal').css('display', 'block');
+    //     $("#zoomImage").prop("src", $(this).attr('src'));
+    // })
 });
